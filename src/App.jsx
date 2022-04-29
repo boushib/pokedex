79698eded16a124050d8317fff1c83bfb8794753
boushib <boushib01@gmail.com>
2022-04-29 02:18:51 +0000

    implement infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useRef, useState } from 'react'
 import api from './api'
 import AppBar from './components/AppBar'
 import Logo from './components/Logo'
-import PokemonList from './components/PokemonList'
+import PokemonCard from './components/PokemonCard'
 import SearchBox from './components/SearchBox'
+
+const ITEMS_PER_PAGE = 20
 
 const App = () => {
   const [allPokemons, setAllPokemons] = useState([])
   const [pokemons, setPokemons] = useState([])
   const [searchQuery, setSearchQuery] = useState('')
+  const [page, setPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
+  const [pagesTotal, setPagesTotal] = useState(0)
+  const observer = useRef()
+  const lastCardRef = useCallback(
+    node => {
+      if (!isLoading) {
+        if (observer.current) observer.current.disconnect()
+
+        observer.current = new IntersectionObserver(entries => {
+          if (entries[0].isIntersecting && page <= pagesTotal) {
+            setPage(p => p + 1)
+          }
+        })
+
+        if (node) observer.current.observe(node)
+      }
+    },
+    [isLoading, pagesTotal, page]
+  )
 
   useEffect(() => {
-    fetchPokemons()
+    if (page <= pagesTotal || pagesTotal === 0) {
+      fetchPokemons()
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, [page])
 
   useEffect(() => {
     setPokemons(
@@ -29,20 +53,27 @@ const App = () => {
   }, [searchQuery])
 
   const fetchPokemons = async () => {
-    const { data } = await api.get(`/pokemon?offset=0&limit=20`)
-    const { results, count } = data
-    const res = await Promise.all(
-      results.map(r => api.get(`/pokemon/${r.name}`))
-    )
-    const pokemons = res.map(r => {
-      const data = r.data
-      const { id, name, types, sprites } = data
-      const image = sprites.other.dream_world.front_default
-      const type = types[0].type.name
-      return { id, name, image, type }
-    })
-    setPokemons(pokemons)
-    setAllPokemons(pokemons)
+    setIsLoading(true)
+    try {
+      const { data } = await api.get(
+        `/pokemon?offset=${ITEMS_PER_PAGE * (page - 1)}&limit=${ITEMS_PER_PAGE}`
+      )
+      const { results, count } = data
+      const res = await Promise.all(
+        results.map(r => api.get(`/pokemon/${r.name}`))
+      )
+      const pokemonsData = res.map(r => {
+        const data = r.data
+        const { id, name, types, sprites } = data
+        const image = sprites.other.dream_world.front_default
+        const type = types[0].type.name
+        return { id, name, image, type }
+      })
+      setPagesTotal(Math.ceil(count / ITEMS_PER_PAGE))
+      setPokemons([...pokemons, ...pokemonsData])
+      setAllPokemons(...pokemons, ...pokemonsData)
+    } catch (error) {}
+    setIsLoading(false)
   }
 
   return (
@@ -55,7 +86,18 @@ const App = () => {
           placeholder="Search by name..."
         />
       </AppBar>
-      <PokemonList pokemons={pokemons} />
+      <div className="pokemon-list grid">
+        {pokemons.map(({ name, image, id, type }, index) => (
+          <PokemonCard
+            name={name}
+            image={image}
+            id={id}
+            type={type}
+            ref={index + 1 === pokemons.length ? lastCardRef : undefined}
+            key={name}
+          />
+        ))}
+      </div>
     </div>
   )
 }
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,15 @@
 import PokemonId from './PokemonId'
 import './PokemonCard.sass'
+import React from 'react'
 
-const PokemonCard = ({ id, name, image, type }) => (
-  <div className="pokemon-card">
+const PokemonCard = React.forwardRef(({ id, name, image, type }, ref) => (
+  <div className="pokemon-card" ref={ref}>
     <img src={image} className="pokemon-card__image" alt="" />
     <div className="pokemon-card__name">
       {name} - <span className={`pokemon-card__type ${type}`}>{type}</span>
     </div>
     <PokemonId id={id} type={type} />
   </div>
-)
+))
 
 export default PokemonCard
