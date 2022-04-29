0fa35c2f5cc5bd2e7bbe5208c9a8a3644cebc34e
boushib <boushib01@gmail.com>
2022-04-29 01:03:43 +0000

    animate pokemon card on hover

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,8 +1,9 @@
 import PokemonCard from './PokemonCard'
+import './PokemonList.sass'
 
 const PokemonList = ({ pokemons }) => {
   return (
-    <div className="pokemon-lis grid">
+    <div className="pokemon-list grid">
       {pokemons.map(({ name, image, id, type }) => (
         <PokemonCard name={name} image={image} id={id} type={type} key={name} />
       ))}
